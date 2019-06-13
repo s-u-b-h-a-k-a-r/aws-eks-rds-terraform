7037ac3dbf606661bbb0f897e603595ae630071b
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-13 22:41:33 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -1,43 +1,26 @@
-resource "null_resource" "k8s-tiller-rbac" {
-  depends_on = ["module.eks"]
-
-  triggers {
-    kube_config_rendered = "${module.eks.kubeconfig}"
-  }
-}
-
 data "aws_eks_cluster" "cluster" {
   depends_on = ["module.eks"]
   name       = "${var.EKS_name}"
 }
 
 data "aws_eks_cluster_auth" "cluster-auth" {
-  depends_on = ["module.eks", "null_resource.k8s-tiller-rbac"]
+  depends_on = ["module.eks"]
   name       = "${var.EKS_name}"
 }
 
 provider "kubernetes" {
-  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
-  token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
-}
-
-resource "kubernetes_namespace" "tiller" {
-  metadata {
-    name = "tiller"
-  }
-  depends_on = ["module.eks", "null_resource.k8s-tiller-rbac"]
-}
-
-module "tiller" {
-  source    = "git::https://github.com/lsst-sqre/terraform-tinfoil-tiller.git?ref=0.9.x"
-  namespace = "${kubernetes_namespace.tiller.metadata.0.name}"
+  host                   = "${aws_eks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)}"
+  token                  = "${data.aws_eks_cluster_auth.this.token}"
+  load_config_file       = false
+  version                = "~> 1.5"
 }
 
 provider "helm" {
-  service_account = "${module.tiller.service_account}"
-  namespace       = "${module.tiller.namespace}"
-  install_tiller  = false
+  namespace       = "kube-system"
+  install_tiller  = true
+  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.14.1"
+  service_account = "tiller"
 
   kubernetes {
     host                   = "${data.aws_eks_cluster.cluster.endpoint}"
@@ -46,12 +29,44 @@ provider "helm" {
   }
 }
 
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  automount_service_account_token = true
+  depends_on                      = ["module.eks"]
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  depends_on = [
+    "kubernetes_service_account.tiller",
+  ]
+}
+
 data "helm_repository" "incubator" {
-  name       = "incubator"
-  url        = "https://kubernetes-charts-incubator.storage.googleapis.com"
+  name = "incubator"
+  url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
 }
 
 data "helm_repository" "stable" {
-  name       = "stable"
-  url        = "https://kubernetes-charts.storage.googleapis.com/"
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com/"
 }
