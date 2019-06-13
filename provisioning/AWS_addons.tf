a7adda94b238e2ebe2efc2a12929803c332d9836
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-13 21:54:22 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -22,11 +22,22 @@ provider "kubernetes" {
   token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
 }
 
+resource "kubernetes_namespace" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+  depends_on = ["module.eks", "null_resource.k8s-tiller-rbac"]
+}
+
+module "tiller" {
+  source    = "git::https://github.com/lsst-sqre/terraform-tinfoil-tiller.git?ref=0.9.x"
+  namespace = "${kubernetes_namespace.tiller.metadata.0.name}"
+}
+
 provider "helm" {
-  namespace       = "kube-system"
-  install_tiller  = true
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.14.1"
-  service_account = "tiller"
+  service_account = "${module.tiller.service_account}"
+  namespace       = "${module.tiller.namespace}"
+  install_tiller  = false
 
   kubernetes {
     host                   = "${data.aws_eks_cluster.cluster.endpoint}"
@@ -35,46 +46,12 @@ provider "helm" {
   }
 }
 
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-
-  automount_service_account_token = true
-  depends_on                      = ["module.eks", "null_resource.k8s-tiller-rbac"]
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-
-  depends_on = [
-    "kubernetes_service_account.tiller",
-  ]
-}
-
 data "helm_repository" "incubator" {
   name       = "incubator"
   url        = "https://kubernetes-charts-incubator.storage.googleapis.com"
-  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
 }
 
 data "helm_repository" "stable" {
   name       = "stable"
   url        = "https://kubernetes-charts.storage.googleapis.com/"
-  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
 }
