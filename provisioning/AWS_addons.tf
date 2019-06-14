a95c4bb4251f3adbcccaa0035438dff97abd6775
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-14 11:32:13 +0530

    Jenkins Changes

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -20,6 +20,7 @@ provider "kubernetes" {
   host                   = "${data.aws_eks_cluster.cluster.endpoint}"
   cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
   token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
+  load_config_file       = false
 }
 
 provider "helm" {
@@ -32,6 +33,7 @@ provider "helm" {
     host                   = "${data.aws_eks_cluster.cluster.endpoint}"
     cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
     token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
+    load_config_file       = false
   }
 }
 
