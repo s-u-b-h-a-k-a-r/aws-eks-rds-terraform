2292fe82fef2473441fbf9826405fbfed13b13fa
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-14 12:18:12 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -20,7 +20,7 @@ provider "kubernetes" {
   host                   = "${data.aws_eks_cluster.cluster.endpoint}"
   cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
   token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
-  load_config_file       = false
+  load_config_file       = true
 }
 
 provider "helm" {
@@ -33,7 +33,7 @@ provider "helm" {
     host                   = "${data.aws_eks_cluster.cluster.endpoint}"
     cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
     token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
-    load_config_file       = false
+    load_config_file       = true
   }
 }
 
