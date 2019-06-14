cb60178854401548d8788a31b20487ac92ca187b
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-14 15:16:05 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -27,7 +27,7 @@ provider "helm" {
     host                   = "${module.eks.cluster_endpoint}"
     cluster_ca_certificate = "${base64decode(module.eks.cluster_certificate_authority_data)}"
     token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
-    load_config_file       = false
+    config_path            = "./${var.EKS_name}_kubeconfig"
   }
 }
 
