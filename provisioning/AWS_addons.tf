fdc560312a9b0a3573eff895a65d2b535ad112ba
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-18 16:29:00 +0530

    Jenkins Support

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -1,13 +1,17 @@
+data "aws_eks_cluster" "cluster" {
+  name = "${module.eks.cluster_id}"
+    depends_on = ["module.eks"]
+}
 data "aws_eks_cluster_auth" "cluster-auth" {
   name       = "${module.eks.cluster_id}"
   depends_on = ["module.eks"]
 }
 
 provider "kubernetes" {
-  host                   = "${module.eks.cluster_endpoint}"
-  cluster_ca_certificate = "${base64decode(module.eks.cluster_certificate_authority_data)}"
+  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
   token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
-  load_config_file       = true
+  load_config_file       = false
 }
 
 resource "kubernetes_service_account" "tiller" {
@@ -48,9 +52,9 @@ provider "helm" {
   namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
 
   kubernetes {
-    host                   = "${module.eks.cluster_endpoint}"
-    cluster_ca_certificate = "${base64decode(module.eks.cluster_certificate_authority_data)}"
-    load_config_file       = true
+  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
+  load_config_file       = false
   }
 }
 data "helm_repository" "incubator" {
