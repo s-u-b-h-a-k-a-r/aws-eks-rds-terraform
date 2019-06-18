1fe17dd97adbe4c8a075d7f3c90a2fc94bf47805
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-18 10:03:11 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -1,13 +1,10 @@
-data "aws_eks_cluster" "cluster" {
-  name = "${module.eks.cluster_id}"
-}
 data "aws_eks_cluster_auth" "cluster-auth" {
   name       = "${module.eks.cluster_id}"
 }
 
 provider "kubernetes" {
-  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
+  host                   = "${module.eks.cluster_endpoint}"
+  cluster_ca_certificate = "${base64decode(module.eks.cluster_certificate_authority_data)}"
   token                  = "${data.aws_eks_cluster_auth.cluster-auth.token}"
   load_config_file       = false
 }
