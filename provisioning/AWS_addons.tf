0cd0ba28889df439b7c15f9c208a33ad0e76d6e3
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-18 22:32:46 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -1,9 +1,5 @@
 resource "null_resource" "k8s-tiller-rbac" {
   depends_on = ["module.eks"]
-
-  triggers {
-    kube_config_rendered = "${module.eks.kubeconfig}"
-  }
 }
 
 data "aws_eks_cluster_auth" "cluster-auth" {
