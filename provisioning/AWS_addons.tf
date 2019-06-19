e7a280591a3e50408b15060d5eedc5ccddeea50f
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-19 11:44:46 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -64,18 +64,6 @@ resource "kubernetes_cluster_role_binding" "tiller" {
   ]
 }
 
-provider "helm" {
-  install_tiller  = true
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.14.0"
-  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-  namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
-
-  kubernetes {
-    load_config_file = true
-    config_path      = "${local_file.kubeconfig.filename}"
-  }
-}
-
 data "helm_repository" "incubator" {
   name       = "incubator"
   url        = "https://kubernetes-charts-incubator.storage.googleapis.com"
