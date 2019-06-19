6232cec767dc48b736e6ac108628f63507bce4e8
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-19 15:59:06 +0530

    Update AWS_addons.tf

--- provisioning/AWS_addons.tf
+++ provisioning/AWS_addons.tf
@@ -33,13 +33,16 @@ resource "kubernetes_cluster_role_binding" "tiller" {
 data "helm_repository" "incubator" {
   name       = "incubator"
   url        = "https://kubernetes-charts-incubator.storage.googleapis.com"
-  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
 }
 
 data "helm_repository" "stable" {
   name       = "stable"
   url        = "https://kubernetes-charts.storage.googleapis.com/"
-  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
+}
+
+data "helm_repository" "pega" {
+  name       = "pega"
+  url        = "https://scrumteamwhitewalkers.github.io/pega-helm-charts/"
 }
 
 resource "helm_release" "mydatabase" {
