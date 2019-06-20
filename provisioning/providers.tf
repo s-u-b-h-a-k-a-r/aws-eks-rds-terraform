b8279a6b0738e2edb88483ba18c7b2b400a6e5fd
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-20 10:17:25 +0530

    Update providers.tf

--- provisioning/providers.tf
+++ provisioning/providers.tf
@@ -2,3 +2,15 @@ provider "aws" {
   version = "~> 2.15"
   region  = "${var.AWS_region}"
 }
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
