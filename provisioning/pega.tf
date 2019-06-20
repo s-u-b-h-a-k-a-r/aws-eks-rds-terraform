2e6d73e755b57a8f97c4cdb6ea322645df0eac51
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-20 18:32:06 +0530

    Jenkins Changes

--- provisioning/pega.tf
+++ provisioning/pega.tf
@@ -3,23 +3,18 @@ data "helm_repository" "pega" {
   url  = "https://scrumteamwhitewalkers.github.io/pega-helm-charts/"
 }
 
-data "template_file" "pega-values" {
-  template = "${file("${path.module}/pega_values.tpl")}"
-
-  vars = {
-    jdbc_url = "jdbc:postgresql://${module.db.this_db_instance_endpoint}/${module.db.this_db_instance_name}"
-  }
-
-  depends_on = ["module.cluster", "module.db"]
-}
-
 resource "helm_release" "pega-installer" {
   namespace  = "${var.namespace}"
   repository = "${data.helm_repository.pega.metadata.0.name}"
   name       = "${var.namespace}"
   chart      = "${var.namespace}"
   version    = "${var.chart_version}"
-  values     = ["${data.template_file.pega-values.rendered}"]
+  values     = ["${file("${path.module}/pega_values.tpl")}"]
   wait       = true
   timeout    = 7200
+
+  set {
+    name  = "jdbc.url"
+    value = "jdbc:postgresql://${module.db.this_db_instance_endpoint}/${module.db.this_db_instance_name}"
+  }
 }
