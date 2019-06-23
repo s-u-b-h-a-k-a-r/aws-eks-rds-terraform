84429569c5e63c2a864c5a334e0435112608b4d7
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-23 15:25:05 +0530

    Update providers.tf

--- provisioning/providers.tf
+++ provisioning/providers.tf
@@ -14,3 +14,22 @@ provider "null" {
 provider "template" {
   version = "~> 2.1"
 }
+
+provider "kubernetes" {
+  version          = "~> 1.7"
+  load_config_file = true
+  config_path      = "./kubeconfig_${module.db.this_db_instance_id}"
+}
+
+provider "helm" {
+  version         = "~> 0.10"
+  install_tiller  = true
+  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.13.1"
+  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
+
+  kubernetes {
+    load_config_file = true
+    config_path      = "./kubeconfig_${module.db.this_db_instance_id}"
+  }
+}
