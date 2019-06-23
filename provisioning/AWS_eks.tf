aa9f6eaae1e131a16a0a63eb2cb3e0aff67a5ea8
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-23 21:30:06 +0530

    Update AWS_eks.tf

--- provisioning/AWS_eks.tf
+++ provisioning/AWS_eks.tf
@@ -10,8 +10,8 @@ module "cluster" {
 
   vpc_id                 = "${module.vpc.vpc_id}"
   worker_groups          = "${var.worker_groups}"
-  cluster_delete_timeout = "30m"
-  cluster_create_timeout = "30m"
+  cluster_delete_timeout = "60m"
+  cluster_create_timeout = "60m"
   manage_aws_auth        = "true"
   write_kubeconfig       = "true"
   config_output_path     = "./"
