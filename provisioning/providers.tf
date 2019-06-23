788114fd7337e1745994f5724f0c5fd3fd482559
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-23 15:45:17 +0530

    Init

--- provisioning/providers.tf
+++ provisioning/providers.tf
@@ -20,16 +20,3 @@ provider "kubernetes" {
   load_config_file = true
   config_path      = "./kubeconfig_${module.db.this_db_instance_id}"
 }
-
-provider "helm" {
-  version         = "~> 0.10"
-  install_tiller  = true
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.13.1"
-  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-  namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
-
-  kubernetes {
-    load_config_file = true
-    config_path      = "./kubeconfig_${module.db.this_db_instance_id}"
-  }
-}
