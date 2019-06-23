a57acb3fe29cd0a9a20b5f3995c2b1d508f648ea
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-23 15:23:12 +0530

    Update pega.tf

--- provisioning/pega.tf
+++ provisioning/pega.tf
@@ -14,23 +14,4 @@ module "pega" {
   docker_url            = "https://index.docker.io/v1/"
   pega_repo_url         = "${var.pega_repo_url}"
   jdbc_url              = "jdbc:postgresql://${module.db.this_db_instance_endpoint}/${module.db.this_db_instance_name}"
-
-  provider "kubernetes" {
-    version          = "~> 1.7"
-    load_config_file = true
-    config_path      = "./kubeconfig_${module.db.this_db_instance_id}"
-  }
-
-  provider "helm" {
-    version         = "~> 0.10"
-    install_tiller  = true
-    tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.13.1"
-    service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-    namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
-
-    kubernetes {
-      load_config_file = true
-      config_path      = "./kubeconfig_${module.db.this_db_instance_id}"
-    }
-  }
 }
