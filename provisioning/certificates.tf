96aabf96ff2274cf4aa8bfd8ec671c6aaf9fa88c
SubhakarKotta <subhakarkotta@gmail.com>
2019-09-12 16:18:46 +0530

    Delete certificates.tf

--- provisioning/certificates.tf
+++
@@ -1,7 +0,0 @@
-module "certificates" {
-  source       = "github.com/scrumteamwhitewalkers/terraform-pega-certificates.git"
-  name         = "${module.cluster.cluster_id}"
-  route53_zone = "${var.route53_zone}"
-  zone_id      = "${var.elb_zone_id}"
-  hostname     = "${module.pega.hostname}"
-}
